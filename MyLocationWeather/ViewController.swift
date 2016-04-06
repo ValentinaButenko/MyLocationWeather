dc3db806913f4ba040575d6d2bdc689a840b23d3
Valentina <v.o.butenko@gmail.com>
2016-04-06 07:48:33 +0300

    Initial Commit

---
+++ MyLocationWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyLocationWeather
+//
+//  Created by Valentina Butenko on 4/6/16.
+//  Copyright © 2016 Valentina Butenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
